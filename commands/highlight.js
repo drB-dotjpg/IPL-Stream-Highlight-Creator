f0a421dc151c99629ec730e68a62992e3e61f01b
Derek <derek.bond24@gmail.com>
2021-12-08 13:40:06 -0600

    Length limiter, better link handling, and i think i got error checking working

--- commands/highlight.js
+++ commands/highlight.js
@@ -3,6 +3,8 @@ const puppeteer = require('puppeteer');
 var ffmpeg = require("fluent-ffmpeg");
 const { MessageAttachment } = require('discord.js');
 
+const fileNameOut = "IPL_Highlight.mp4";
+
 //create the data found in the slash command
 const data = new SlashCommandBuilder()
     .setName("highlight")
@@ -24,38 +26,45 @@ const data = new SlashCommandBuilder()
 
 function ffmpegOverlayer(file, tourney) {
     return new Promise((resolve, reject) => {
-        ffmpeg().withOptions([
-            "-i " + file, //take the twitch clip as an input
-            "-c:v libvpx-vp9", //encode it in a way that makes this work (idk how it works)
-            "-i overlay.webm", //take the overlay as an input
-            ])
-            .complexFilter([
-              {
-                "filter":"scale", "options":{s:"1280x720"}, "inputs":"[0:v]", "outputs":"[base]" //resize the twitch clip to 720p
-              },
-              {
-                "filter":"overlay", "inputs":"[base][1:v]" //overlay the overlay onto the twitch clip
-              }
-            ])
-            .withOptions([
-              "-r 30", //set fps to 30
-              "-b:a 96k", //compress the audio via bitrate
-              "-crf 26", //compress the video
-              "-c:v h264_qsv" //encode the video
-            ])
-        .on('start', function(){
-            console.log("starting ffmpeg with input " + file + " and overlay " + tourney);
-        })
-        .on('error', function(err) {
-            console.log('An ffmpeg error occurred: ' + err.message);
-            return reject(new Error(err));
-        })
-        .on('end', function(){
-            console.log("ffmpeg is done!");
-            resolve();
-        })
-        .save("ffmpegtester.mp4");
-        
+
+        ffmpeg.ffprobe(file, function(error, metadata) {
+            const clipDuration = metadata.format.duration;
+            if (clipDuration > 35){
+                reject("Clip is too long (clips should be under 35 seconds long)");
+            } else {
+                ffmpeg().withOptions([
+                    "-i " + file, //take the twitch clip as an input
+                    "-c:v libvpx-vp9", //encode it in a way that makes this work (idk how it works)
+                    "-i ./overlays/" + tourney + ".webm", //take the overlay as an input
+                    ])
+                    .complexFilter([
+                      {
+                        "filter":"scale", "options":{s:"1280x720"}, "inputs":"[0:v]", "outputs":"[base]" //resize the twitch clip to 720p
+                      },
+                      {
+                        "filter":"overlay", "inputs":"[base][1:v]" //overlay the overlay onto the twitch clip
+                      }
+                    ])
+                    .withOptions([
+                      "-r 30", //set fps to 30
+                      "-b:a 96k", //compress the audio via bitrate
+                      "-crf 26", //compress the video
+                      "-c:v h264_qsv" //encode the video
+                    ])
+                .on('start', function(){
+                    console.log("starting ffmpeg with input " + file + " and overlay " + tourney);
+                })
+                .on('error', function(err) {
+                    console.log('An ffmpeg error occurred: ' + err.message);
+                    return reject(new Error(err));
+                })
+                .on('end', function(){
+                    console.log("ffmpeg is done!");
+                    resolve();
+                })
+                .save(fileNameOut);
+            }
+        });
     })
 }
 
@@ -68,9 +77,9 @@ module.exports = {
         const link = interaction.options.getString('link');
         
         //make sure this is a twitch link
-        if (!link.match(/https:\/\/www\.twitch\.tv\/[A-Za-z0-9]{1,}/)){
+        if (!link.match(/https:\/\/www\.twitch\.tv\/iplsplatoon\/clip\/[A-Za-z0-9]{1,}/)){ //https://www.twitch.tv/iplsplatoon/clip/
             await interaction.reply({
-                content:"**Invalid link.**\nPlease enter a twitch clip link!",
+                content:"**Invalid link.**\nPlease enter an IPL twitch clip link!",
                 ephemeral: true
             });
             return;
@@ -80,17 +89,22 @@ module.exports = {
         await interaction.deferReply();
 
         //launch a headless browser and load the twitch page
-        console.log("launching headless browser.");
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto(link, { waitUntil: 'networkidle0' });
-        console.log("page loaded, finding and getting video source.");
+        try{
+            console.log("launching headless browser.");
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto(link, { waitUntil: 'networkidle0' });
+            console.log("page loaded, finding and getting video source.");
 
-        //get the source link of the video
-        const source = await page.evaluate(() => {
-            return document.querySelector("video").currentSrc;
-        });
-        console.log("got video source")
+            //get the source link of the video
+            const source = await page.evaluate(() => {
+                return document.querySelector("video").currentSrc;
+            });
+            console.log("got video source " + source);
+        } catch (err) {
+            await interaction.editReply("The was an error getting this clip from twitch!\n`" + err + "`");
+            return;
+        }
 
         //we gotta change the link a bit before we can process it
         var vidIdSplit = source.split('?')[0].split('/');
@@ -98,15 +112,15 @@ module.exports = {
         const clipLink = "https://clips-media-assets2.twitch.tv/" + vidId;
         console.log("The generated clip link is " + clipLink);
         
-
         //ffmpeg time
         await ffmpegOverlayer(clipLink, tourney)
             .then(() => {
                 console.log("Uploading...");
-                const file = new MessageAttachment("ffmpegtester.mp4");
+                const file = new MessageAttachment(fileNameOut);
                 interaction.editReply({files:[file]});
+            }).catch((err) => {
+                console.log("ffmpegOverlayer was rejected: " + err);
+                interaction.editReply("There was an error processing this clip!\n`" + err + "`");
             });
-
-        
     },
 };
