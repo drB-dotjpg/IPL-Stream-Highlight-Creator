e148ba585b3be981f1d3b7bc2c4e26408094f916
Derek Bond <derek.bond24@gmail.com>
2022-04-20 12:15:34 -0500

    adding ltc support

--- commands/highlight.js
+++ commands/highlight.js
@@ -30,6 +30,7 @@ const data = new SlashCommandBuilder()
             .addChoice("King of the Castle", "king_of_the_castle")
             .addChoice("Splatoon Advanced Circuit", "sac")
             .addChoice("Squidboards Splat Series", "sqss")
+            .addChoice("Low Tide City", "ltc")
     )
     .addStringOption(option =>
         option.setName('link')
